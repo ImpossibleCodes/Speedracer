c262f6a5074a72a67d532ba5215684335dfcacbe
Neil Agrawal <neilagrawal@Neils-MacBook-Pro.local>
2022-05-19 08:46:54 -0400

    Double Point Lidar Working

--- Testing/PointLidar/PointLidar.ino
+++ Testing/PointLidar/PointLidar.ino
@@ -1,16 +1,20 @@
 // Updated ESP + Arduino Sample Code
 #include "TFMPlus.h" // Include TFMini Plus Library v1.5.0
-TFMPlus tfmP_r;        // Create a TFMini Plus object
-TFMPlus tfmP_l;        // Create a TFMini Plus object
+TFMPlus tfmP_l;      // Create a TFMini Plus object
+TFMPlus tfmP_r;      // Create a TFMini Plus object
+
+static const int FRAMERATE = FRAME_50;
 
 void setup()
 {
     Serial.begin(115200);                                       // Intialize terminal serial port
     delay(20);                                                  // Give port time to initalize
     Serial.println("\r\nTFMPlus Library Example w/ ESP32\r\n"); // say 'hello'
-    Serial2.begin(115200, SERIAL_8N1, 17, 16);                  // 5, 18 | 17, 16
+    Serial1.begin(115200, SERIAL_8N1, 5, 18);                   // 5, 18
+    Serial2.begin(115200, SERIAL_8N1, 17, 16);                  // 17, 16
     delay(20);                                                  // Give port time to initalize
-    tfmP_r.begin(&Serial2);                                       // Initialize device library object and...
+    tfmP_l.begin(&Serial1);                                     // Initialize device library object and...
+    tfmP_r.begin(&Serial2);                                     // Initialize device library object and...
                                                                 // pass device serial port to the object.
     // Send commands to the TFMiniPlus
     // - - Perform a system reset - - - - - - - - - - -
@@ -20,14 +24,26 @@ void setup()
         Serial.println("PASSED\r\n");
     }
     else
+    {
+        Serial.print("L: ");
+        tfmP_l.printReply();
+        Serial.print("R: ");
         tfmP_r.printReply();
+    }
 
     delay(500); // added to allow the System Rest enough time to complete
 
     // - - Display the firmware version - - - - - - - - -
     if (tfmP_r.sendCommand(GET_FIRMWARE_VERSION, 0))
     {
-        Serial.println("Firmware Version: ");
+        Serial.println("L Firmware Version: ");
+        Serial.print(tfmP_l.version[0]); // print three single numbers
+        Serial.print(".");
+        Serial.print(tfmP_l.version[1]);
+        Serial.print(".");
+        Serial.print(tfmP_l.version[2]);
+        Serial.println();
+        Serial.println("R Firmware Version: ");
         Serial.print(tfmP_r.version[0]); // print three single numbers
         Serial.print(".");
         Serial.print(tfmP_r.version[1]);
@@ -36,18 +52,36 @@ void setup()
         Serial.println();
     }
     else
+    {
+        tfmP_l.printReply();
         tfmP_r.printReply();
+    }
 
     // - M- Set the data frame-rate to 5Hz - - - - - - - -
-    Serial.println("Data-Frame rate: ");
-    if (tfmP_r.sendCommand(SET_FRAME_RATE, FRAME_50))
+    Serial.println("L Data-Frame rate: ");
+    if (tfmP_l.sendCommand(SET_FRAME_RATE, FRAMERATE))
     {
-        Serial.println(FRAME_50);
+        Serial.println(FRAMERATE);
+    }
+    else
+        tfmP_l.printReply();
+
+    Serial.println("R Data-Frame rate: ");
+    if (tfmP_r.sendCommand(SET_FRAME_RATE, FRAMERATE))
+    {
+        Serial.println(FRAMERATE);
     }
     else
         tfmP_r.printReply();
 
     delay(500);
+
+    if (tfmP_l.sendCommand(STANDARD_FORMAT_CM, 0))
+    {
+        Serial.println("CM");
+    }
+    else
+        tfmP_l.printReply();
 
     if (tfmP_r.sendCommand(STANDARD_FORMAT_CM, 0))
     {
@@ -61,23 +95,31 @@ void setup()
 }
 
 // Initialize variables
-int16_t tfDist = 0; // Distance to object in centimeters
-int16_t tfFlux = 0; // Strength or quality of return signal
-int16_t tfTemp = 0; // Internal temperature of Lidar sensor chip
+int16_t tfDist_l = 0; // Distance to object in centimeters
+int16_t tfFlux_l = 0; // Strength or quality of return signal
+int16_t tfTemp_l = 0; // Internal temperature of Lidar sensor chip
+int16_t tfDist_r = 0; // Distance to object in centimeters
+int16_t tfFlux_r = 0; // Strength or quality of return signal
+int16_t tfTemp_r = 0; // Internal temperature of Lidar sensor chip
 
 void loop()
 {
-    delay(20); // Loop delay to match the 5Hz data frame rate  - Use: 1/(FRAME_RATE) * 1000
+    delay(1 / FRAMERATE * 1000); // Loop delay to match the 5Hz data frame rate  - Use: 1/(FRAME_RATE) * 1000
 
-    if (tfmP_r.getData(tfDist, tfFlux, tfTemp))
-    {                                      // Get data from the device.
-        printf("Dist:%04icm ", tfDist);    // display distance,
-        printf("Flux:%05i ", tfFlux);      // display signal strength/quality,
-        printf("Temp:%2i%s", tfTemp, "C"); // display temperature,
-        printf("\r\n");                    // end-of-line.
+    if (tfmP_l.getData(tfDist_l, tfFlux_l, tfTemp_l) && tfmP_r.getData(tfDist_r, tfFlux_r, tfTemp_r))
+    {                                          // Get data from the device.
+        printf("L Dist:%04icm ", tfDist_l);    // display distance,
+        printf("L Flux:%05i ", tfFlux_l);      // display signal strength/quality,
+        printf("L Temp:%2i%s", tfTemp_l, "C"); // display temperature,
+        printf("\r");                        // end-of-line.
+        printf("R Dist:%04icm ", tfDist_r);    // display distance,
+        printf("R Flux:%05i ", tfFlux_r);      // display signal strength/quality,
+        printf("R Temp:%2i%s", tfTemp_r, "C"); // display temperature,
+        printf("\r\n");                      // end-of-line.
     }
     else
-    {                      // If the command fails...
-        tfmP_r.printFrame(); // display the error and HEX dataa
+    {                        // If the command fails...
+        tfmP_l.printFrame(); // display the error and HEX data
+        tfmP_r.printFrame(); // display the error and HEX data
     }
 }
--- build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -1,83 +1,126 @@
 # 1 "/Users/neilagrawal/Projects/Speedracer/Testing/PointLidar/PointLidar.ino"
 // Updated ESP + Arduino Sample Code
 # 3 "/Users/neilagrawal/Projects/Speedracer/Testing/PointLidar/PointLidar.ino" 2
-TFMPlus tfmP; // Create a TFMini Plus object
+TFMPlus tfmP_l; // Create a TFMini Plus object
+TFMPlus tfmP_r; // Create a TFMini Plus object
+
+static const int FRAMERATE = 0x0032;
 
 void setup()
 {
     Serial.begin(115200); // Intialize terminal serial port
     delay(20); // Give port time to initalize
     Serial.println("\r\nTFMPlus Library Example w/ ESP32\r\n"); // say 'hello'
-    Serial2.begin(115200, 0x800001c, 17, 16); // 5, 18 | 17, 16
+    Serial1.begin(115200, 0x800001c, 5, 18); // 5, 18
+    Serial2.begin(115200, 0x800001c, 17, 16); // 17, 16
     delay(20); // Give port time to initalize
-    tfmP.begin(&Serial2); // Initialize device library object and...
+    tfmP_l.begin(&Serial1); // Initialize device library object and...
+    tfmP_r.begin(&Serial2); // Initialize device library object and...
                                                                 // pass device serial port to the object.
     // Send commands to the TFMiniPlus
     // - - Perform a system reset - - - - - - - - - - -
     Serial.println("Soft reset: ");
-    if (tfmP.sendCommand(0x00020405 /* returns a 1 byte pass/fail (0/1)*/, 0))
+    if (tfmP_r.sendCommand(0x00020405 /* returns a 1 byte pass/fail (0/1)*/, 0))
     {
         Serial.println("PASSED\r\n");
     }
     else
-        tfmP.printReply();
+    {
+        Serial.print("L: ");
+        tfmP_l.printReply();
+        Serial.print("R: ");
+        tfmP_r.printReply();
+    }
 
     delay(500); // added to allow the System Rest enough time to complete
 
     // - - Display the firmware version - - - - - - - - -
-    if (tfmP.sendCommand(0x00010407 /* returns 3 byte firmware version*/, 0))
+    if (tfmP_r.sendCommand(0x00010407 /* returns 3 byte firmware version*/, 0))
     {
-        Serial.println("Firmware Version: ");
-        Serial.print(tfmP.version[0]); // print three single numbers
+        Serial.println("L Firmware Version: ");
+        Serial.print(tfmP_l.version[0]); // print three single numbers
         Serial.print(".");
-        Serial.print(tfmP.version[1]);
+        Serial.print(tfmP_l.version[1]);
         Serial.print(".");
-        Serial.print(tfmP.version[2]);
+        Serial.print(tfmP_l.version[2]);
+        Serial.println();
+        Serial.println("R Firmware Version: ");
+        Serial.print(tfmP_r.version[0]); // print three single numbers
+        Serial.print(".");
+        Serial.print(tfmP_r.version[1]);
+        Serial.print(".");
+        Serial.print(tfmP_r.version[2]);
         Serial.println();
     }
     else
-        tfmP.printReply();
+    {
+        tfmP_l.printReply();
+        tfmP_r.printReply();
+    }
 
     // - M- Set the data frame-rate to 5Hz - - - - - - - -
-    Serial.println("Data-Frame rate: ");
-    if (tfmP.sendCommand(0x00030606 /* These commands each return*/, 0x0032))
+    Serial.println("L Data-Frame rate: ");
+    if (tfmP_l.sendCommand(0x00030606 /* These commands each return*/, FRAMERATE))
     {
-        Serial.println(0x0032);
+        Serial.println(FRAMERATE);
     }
     else
-        tfmP.printReply();
+        tfmP_l.printReply();
+
+    Serial.println("R Data-Frame rate: ");
+    if (tfmP_r.sendCommand(0x00030606 /* These commands each return*/, FRAMERATE))
+    {
+        Serial.println(FRAMERATE);
+    }
+    else
+        tfmP_r.printReply();
 
     delay(500);
 
-    if (tfmP.sendCommand(0x01050505 /*           "*/, 0))
+    if (tfmP_l.sendCommand(0x01050505 /*           "*/, 0))
     {
         Serial.println("CM");
     }
     else
-        tfmP.printReply();
+        tfmP_l.printReply();
+
+    if (tfmP_r.sendCommand(0x01050505 /*           "*/, 0))
+    {
+        Serial.println("CM");
+    }
+    else
+        tfmP_r.printReply();
 
     // - - - - - - - - - - - - - - - - - - - - - - - -
     delay(500); // And wait for half a second.
 }
 
 // Initialize variables
-int16_t tfDist = 0; // Distance to object in centimeters
-int16_t tfFlux = 0; // Strength or quality of return signal
-int16_t tfTemp = 0; // Internal temperature of Lidar sensor chip
+int16_t tfDist_l = 0; // Distance to object in centimeters
+int16_t tfFlux_l = 0; // Strength or quality of return signal
+int16_t tfTemp_l = 0; // Internal temperature of Lidar sensor chip
+int16_t tfDist_r = 0; // Distance to object in centimeters
+int16_t tfFlux_r = 0; // Strength or quality of return signal
+int16_t tfTemp_r = 0; // Internal temperature of Lidar sensor chip
 
 void loop()
 {
-    delay(20); // Loop delay to match the 5Hz data frame rate  - Use: 1/(FRAME_RATE) * 1000
+    delay(1 / FRAMERATE * 1000); // Loop delay to match the 5Hz data frame rate  - Use: 1/(FRAME_RATE) * 1000
 
-    if (tfmP.getData(tfDist, tfFlux, tfTemp))
+    if (tfmP_l.getData(tfDist_l, tfFlux_l, tfTemp_l) && tfmP_r.getData(tfDist_r, tfFlux_r, tfTemp_r))
     { // Get data from the device.
-        printf("Dist:%04icm ", tfDist); // display distance,
-        printf("Flux:%05i ", tfFlux); // display signal strength/quality,
-        printf("Temp:%2i%s", tfTemp, "C"); // display temperature,
+        printf("L Dist:%04icm ", tfDist_l); // display distance,
+        printf("L Flux:%05i ", tfFlux_l); // display signal strength/quality,
+        printf("L Temp:%2i%s", tfTemp_l, "C"); // display temperature,
+        printf("\r"); // end-of-line.
+        printf("R Dist:%04icm ", tfDist_r); // display distance,
+        printf("R Flux:%05i ", tfFlux_r); // display signal strength/quality,
+        printf("R Temp:%2i%s", tfTemp_r, "C"); // display temperature,
         printf("\r\n"); // end-of-line.
     }
     else
     { // If the command fails...
-        tfmP.printFrame(); // display the error and HEX dataa
+        tfmP_l.printFrame(); // display the error and HEX data
+        tfmP_r.printFrame(); // display the error and HEX data
     }
 }
